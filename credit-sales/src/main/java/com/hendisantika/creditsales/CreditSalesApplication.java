84fd940ddfcaab88dbae105f67fc0c6b5b02c198
hendisantika <hendisantika@yahoo.co.id>
2019-04-29 04:04:16 +0700

    #3 - Add credit-sales module

---
+++ credit-sales/src/main/java/com/hendisantika/creditsales/CreditSalesApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.creditsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditSalesApplication.class, args);
+    }
+
+}
