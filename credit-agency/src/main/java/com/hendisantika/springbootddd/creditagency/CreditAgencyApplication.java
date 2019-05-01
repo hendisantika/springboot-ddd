415fde7a85af96832ce0d26561744bf43f57fb1e
hendisantika <hendisantika@yahoo.co.id>
2019-05-02 06:21:43 +0700

    #47 - Add credit-agency module

---
+++ credit-agency/src/main/java/com/hendisantika/springbootddd/creditagency/CreditAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootddd.creditagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditAgencyApplication.class, args);
+    }
+
+}
