548fe71b5730c277987b37c3a699bec9f3d9c690
hendisantika <hendisantika@yahoo.co.id>
2019-05-04 08:49:30 +0700

    #63 - Add buildFeedMetadata method in PersonRatingAtomFeedView in credit-agency module

--- credit-agency/src/main/java/com/hendisantika/springbootddd/creditagency/controller/PersonRatingAtomFeedView.java
+++ credit-agency/src/main/java/com/hendisantika/springbootddd/creditagency/controller/PersonRatingAtomFeedView.java
@@ -1,7 +1,17 @@
 package com.hendisantika.springbootddd.creditagency.controller;
 
 import com.hendisantika.springbootddd.creditagency.repository.PersonRatingRepository;
+import com.rometools.rome.feed.atom.Content;
+import com.rometools.rome.feed.atom.Feed;
+import com.rometools.rome.feed.atom.Link;
+import com.rometools.rome.feed.atom.Person;
+import com.rometools.rome.feed.synd.SyndPerson;
 import org.springframework.web.servlet.view.feed.AbstractAtomFeedView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +28,31 @@ public class PersonRatingAtomFeedView extends AbstractAtomFeedView {
     public PersonRatingAtomFeedView(PersonRatingRepository personRatingRepository) {
         this.personRatingRepository = personRatingRepository;
     }
+
+    protected void buildFeedMetadata(Map<String, Object> model, Feed feed, HttpServletRequest request) {
+        feed.setId("https://github.com/mploed/ddd-with-spring/credit-agency");
+        feed.setTitle("Credit Agency Ratings");
+        List<Link> alternateLinks = new ArrayList<>();
+        Link link = new Link();
+        link.setRel("self");
+        link.setHref(baseUrl(request) + "feed");
+        alternateLinks.add(link);
+        List<SyndPerson> authors = new ArrayList<SyndPerson>();
+        Person person = new Person();
+        person.setName("Big Pug Bank");
+        authors.add(person);
+        feed.setAuthors(authors);
+
+        feed.setAlternateLinks(alternateLinks);
+        feed.setUpdated(personRatingRepository.lastUpdate());
+        Content subtitle = new Content();
+        subtitle.setValue("List of all valid person ratings");
+        feed.setSubtitle(subtitle);
+    }
+
+    private String baseUrl(HttpServletRequest request) {
+        return String.format("%s://%s:%d%s/", request.getScheme(), request.getServerName(), request.getServerPort(),
+                request.getContextPath());
+    }
+
 }
