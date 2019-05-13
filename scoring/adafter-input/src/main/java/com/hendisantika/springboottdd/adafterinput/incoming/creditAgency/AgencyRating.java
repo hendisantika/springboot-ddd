3246e07921de44d89642516605e4ed57e370b4b0
hendisantika <hendisantika@yahoo.co.id>
2019-05-14 05:47:13 +0700

    #75 - Add AgencyRating Model in adapter-input module

---
+++ scoring/adafter-input/src/main/java/com/hendisantika/springboottdd/adafterinput/incoming/creditAgency/AgencyRating.java
@@ -0,0 +1,68 @@
+package com.hendisantika.springboottdd.adafterinput.incoming.creditAgency;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-14
+ * Time: 05:46
+ */
+public class AgencyRating {
+    private int points;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String postCode;
+    private String city;
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
+
