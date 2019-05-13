8dce4b766240cce7af62b9b9de5bef44a94d2f74
hendisantika <hendisantika@yahoo.co.id>
2019-05-13 23:16:29 +0700

    #69 - Add adapter-input in score as new module

---
+++ scoring/adafter-input/src/main/java/com/hendisantika/springboottdd/adafterinput/AdafterInputApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboottdd.adafterinput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdafterInputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdafterInputApplication.class, args);
+    }
+
+}
---
+++ scoring/adafter-input/src/test/java/com/hendisantika/springboottdd/adafterinput/AdafterInputApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springboottdd.adafterinput;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdafterInputApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
