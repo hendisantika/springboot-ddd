e4e3127307017c29c5f0720239d0903a3d8270cb
hendisantika <hendisantika@yahoo.co.id>
2019-05-15 04:33:11 +0700

    #92 - Add realEstate/Construction Model in scoring/adapter-input module

---
+++ scoring/adafter-input/src/main/java/com/hendisantika/springboottdd/adafterinput/realEstate/Construction.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springboottdd.adafterinput.realEstate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-15
+ * Time: 04:32
+ */
+public enum Construction {
+    PREFABRICATED_HOUSE("Prefabricated house"),
+    WOOD("Wood"),
+    STONE("Stone");
+
+    private final String displayName;
+
+    Construction(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
