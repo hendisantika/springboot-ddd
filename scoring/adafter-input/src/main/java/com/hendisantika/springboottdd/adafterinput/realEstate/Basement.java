f9833fabdaef36c53e99567451253d7c9bf94046
hendisantika <hendisantika@yahoo.co.id>
2019-05-15 04:32:48 +0700

    #91 - Add realEstate/Basement Model in scoring/adapter-input module

---
+++ scoring/adafter-input/src/main/java/com/hendisantika/springboottdd/adafterinput/realEstate/Basement.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springboottdd.adafterinput.realEstate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-15
+ * Time: 04:32
+ */
+public enum Basement {
+    NOT_PRESENT("Not present"),
+    PARTLY_DEVELOPED("Partly developed"),
+    FULLY_DEVELOPED("Fully developed");
+
+    private final String displayName;
+
+    Basement(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
