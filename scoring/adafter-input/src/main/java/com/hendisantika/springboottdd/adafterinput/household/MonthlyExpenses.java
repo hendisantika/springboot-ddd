e7fa6a0980176d04714f5ebb6ba9610ed15961f9
hendisantika <hendisantika@yahoo.co.id>
2019-05-15 04:30:28 +0700

    #86 - Add house/MonthlyExpenses Model in scoring/adapter-input module

---
+++ scoring/adafter-input/src/main/java/com/hendisantika/springboottdd/adafterinput/household/MonthlyExpenses.java
@@ -0,0 +1,79 @@
+package com.hendisantika.springboottdd.adafterinput.household;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-15
+ * Time: 04:30
+ */
+public class MonthlyExpenses implements Serializable {
+    private int healthInsuranceFirstApplicant;
+    private int healthInsuranceSecondApplicant;
+    private int otherLoansRemainderOfDebt;
+    private int otherLoansMonthlyRepayments;
+    private int costOfLiving;
+    private int rent;
+    private boolean rentNotApplicableInFuture;
+
+    public int getHealthInsuranceFirstApplicant() {
+        return healthInsuranceFirstApplicant;
+    }
+
+    public void setHealthInsuranceFirstApplicant(int healthInsuranceFirstApplicant) {
+        this.healthInsuranceFirstApplicant = healthInsuranceFirstApplicant;
+    }
+
+    public int getHealthInsuranceSecondApplicant() {
+        return healthInsuranceSecondApplicant;
+    }
+
+    public void setHealthInsuranceSecondApplicant(int healthInsuranceSecondApplicant) {
+        this.healthInsuranceSecondApplicant = healthInsuranceSecondApplicant;
+    }
+
+    public int getOtherLoansRemainderOfDebt() {
+        return otherLoansRemainderOfDebt;
+    }
+
+    public void setOtherLoansRemainderOfDebt(int otherLoansRemainderOfDebt) {
+        this.otherLoansRemainderOfDebt = otherLoansRemainderOfDebt;
+    }
+
+    public int getOtherLoansMonthlyRepayments() {
+        return otherLoansMonthlyRepayments;
+    }
+
+    public void setOtherLoansMonthlyRepayments(int otherLoansMonthlyRepayments) {
+        this.otherLoansMonthlyRepayments = otherLoansMonthlyRepayments;
+    }
+
+    public int getCostOfLiving() {
+        return costOfLiving;
+    }
+
+    public void setCostOfLiving(int costOfLiving) {
+        this.costOfLiving = costOfLiving;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public void setRent(int rent) {
+        this.rent = rent;
+    }
+
+    public boolean isRentNotApplicableInFuture() {
+        return rentNotApplicableInFuture;
+    }
+
+    public void setRentNotApplicableInFuture(boolean rentNotApplicableInFuture) {
+        this.rentNotApplicableInFuture = rentNotApplicableInFuture;
+    }
+}
+
