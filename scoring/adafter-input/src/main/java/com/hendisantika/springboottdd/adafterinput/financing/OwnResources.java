e3911db526a2a037606d0585fc184e3db1d21e20
hendisantika <hendisantika@yahoo.co.id>
2019-05-15 04:28:26 +0700

    #83 - Add financing/OwnResources Model in scoring/adapter-input module

---
+++ scoring/adafter-input/src/main/java/com/hendisantika/springboottdd/adafterinput/financing/OwnResources.java
@@ -0,0 +1,46 @@
+package com.hendisantika.springboottdd.adafterinput.financing;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-15
+ * Time: 04:28
+ */
+public class OwnResources implements Serializable {
+    private int liquidAssets;
+    private int balanceFromBuildingSociety;
+    private int ownManpower;
+
+    public int sum() {
+        return liquidAssets + balanceFromBuildingSociety + ownManpower;
+    }
+
+    public int getLiquidAssets() {
+        return liquidAssets;
+    }
+
+    public void setLiquidAssets(int liquidAssets) {
+        this.liquidAssets = liquidAssets;
+    }
+
+    public int getBalanceFromBuildingSociety() {
+        return balanceFromBuildingSociety;
+    }
+
+    public void setBalanceFromBuildingSociety(int balanceFromBuildingSociety) {
+        this.balanceFromBuildingSociety = balanceFromBuildingSociety;
+    }
+
+    public int getOwnManpower() {
+        return ownManpower;
+    }
+
+    public void setOwnManpower(int ownManpower) {
+        this.ownManpower = ownManpower;
+    }
+}
