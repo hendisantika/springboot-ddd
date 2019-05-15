ee8e763e958c0b570ba4a2b219e8967ba64d86c4
hendisantika <hendisantika@yahoo.co.id>
2019-05-16 06:31:23 +0700

    #95 - Add realEstate/ObjectType Model in scoring/adapter-input module

---
+++ scoring/adafter-input/src/main/java/com/hendisantika/springboottdd/adafterinput/realEstate/ObjectType.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springboottdd.adafterinput.realEstate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-16
+ * Time: 06:31
+ */
+public enum ObjectType {
+    SEMI_DETACHED_HOUSE("Semi detached house"),
+    ROW_HOUSE("Row house"),
+    DETACHED_HOUSE("Detached house"),
+    APARTMENT("Apartment");
+
+    private final String displayName;
+
+    ObjectType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
