d2ea1a17034f73e258be41eaaef779fbfb04b6a2
hendisantika <hendisantika@yahoo.co.id>
2019-05-16 06:30:11 +0700

    #93 - Add realEstate/Feature Model in scoring/adapter-input module

---
+++ scoring/adafter-input/src/main/java/com/hendisantika/springboottdd/adafterinput/realEstate/Feature.java
@@ -0,0 +1,32 @@
+package com.hendisantika.springboottdd.adafterinput.realEstate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ddd
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-16
+ * Time: 06:29
+ */
+public enum Feature {
+    SOLAR_COLLECTORS("Solar collectors"),
+    PHOTOVOLTAICS("Photovoltaics"),
+    SPA("Spa"),
+    TWO_OR_MORE_BATHS("Two or more baths"),
+    EV_CHARGER("Charger for electric vehicle"),
+    POOL("Pool"),
+    SUMMERHOUSE_IN_GARDEN("Summerhouse in the garden"),
+    SAT_TV("Satellite TV");
+
+    private final String displayName;
+
+    Feature(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+}
