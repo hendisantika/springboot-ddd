2284f00f49b4bf08392622211e6bf922ae585b04
hendisantika <hendisantika@yahoo.co.id>
2019-05-24 05:25:31 +0700

    #106 - Add scoring/adapter-output as new module

---
+++ scoring/adapters-output/src/main/java/com/hendisantika/springbootddd/scoring/adaptersoutput/AdaptersOutputApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootddd.scoring.adaptersoutput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptersOutputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptersOutputApplication.class, args);
+    }
+
+}
---
+++ scoring/adapters-output/src/test/java/com/hendisantika/springbootddd/scoring/adaptersoutput/AdaptersOutputApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootddd.scoring.adaptersoutput;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdaptersOutputApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
