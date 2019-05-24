6d356851b751ccef03c80640a4a6e82830f643f5
hendisantika <hendisantika@yahoo.co.id>
2019-05-25 06:35:50 +0700

    #118 - Add sharedmodel as new module

---
+++ scoring/sharedmodel/src/test/java/com/hendisantika/springbootddd/sharedmodel/SharedmodelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootddd.sharedmodel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SharedmodelApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
